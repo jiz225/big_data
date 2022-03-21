89d0b2eec7a5116c9a25d8c629629ab113453854
Jingxiao Zhang <jingxiaozhang@Jingxiaos-MacBook-Pro.local>
2022-03-20 23:34:42 -0500

    HW2

---
+++ HW2/HW2_HBase.java
@@ -0,0 +1,148 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.*;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+import java.io.IOException;
+import java.util.List;
+public class HW2_HBase{
+    public static void putDataRow(Connection connection, TableName tableName, String row, String columns, String cell, String value) throws IOException{
+        Put put = new Put(Bytes.toBytes(row));
+        put.addColumn(Bytes.toBytes(columns), Bytes.toBytes(cell), Bytes.toBytes(value));
+
+        connection.getTable(tableName).put(put);
+    }
+    public static boolean findNamespace(Admin admin, String ns) throws IOException {
+        NamespaceDescriptor[] nsDescriptors = admin.listNamespaceDescriptors();
+        boolean find_ns = false;
+        for (NamespaceDescriptor nsDescriptor : nsDescriptors) {
+            if (nsDescriptor.getName().equals(ns)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) throws IOException {
+        Configuration configuration = HBaseConfiguration.create();
+        configuration.set("hbase.zookeeper.quorum", "emr-worker-2,emr-worker-1,emr-header-1");
+        configuration.set("hbase.zookeeper.property.clientPort", "2181");
+        configuration.set("hbase.master", "127.0.0.1:60000");
+        Connection conn = ConnectionFactory.createConnection(configuration);
+        Admin admin = conn.getAdmin();
+
+        String ns = "zhangj";
+        String tb = "student";
+
+        if (!findNamespace(admin, ns)) {
+            NamespaceDescriptor nsDescriptor = NamespaceDescriptor.create(ns).build();
+            System.out.println("Namespace create successful");
+        } else {
+            System.out.println("Namespace " + ns + " already exists");
+        }
+
+        TableName tableName = TableName.valueOf(ns + ":" + tb);
+        String[] colFamilies = {"info", "score"};
+        if (admin.tableExists(tableName)) {
+            System.out.println("Table " + tb + " already exists");
+        } else {
+            HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
+            for (String colFamily : colFamilies) {
+                HColumnDescriptor hColumnDescriptor = new HColumnDescriptor(colFamily);
+                hTableDescriptor.addFamily(hColumnDescriptor);
+            }
+            admin.createTable(hTableDescriptor);
+            System.out.println("Table create successful");
+        }
+
+        putDataRow(conn, tableName, "Tom", "info", "student_id", "20210000000001");
+        putDataRow(conn, tableName, "Tom", "info", "class", "1");
+        putDataRow(conn, tableName, "Tom", "score", "understanding", "75");
+        putDataRow(conn, tableName, "Tom", "score", "programming", "82");
+
+        putDataRow(conn, tableName, "Jerry", "info", "student_id", "20210000000002");
+        putDataRow(conn, tableName, "Jerry", "info", "class", "1");
+        putDataRow(conn, tableName, "Jerry", "score", "understanding", "85");
+        putDataRow(conn, tableName, "Jerry", "score", "programming", "67");
+
+        putDataRow(conn, tableName, "Jack", "info", "student_id", "20210000000003");
+        putDataRow(conn, tableName, "Jack", "info", "class", "2");
+        putDataRow(conn, tableName, "Jack", "score", "understanding", "80");
+        putDataRow(conn, tableName, "Jack", "score", "programming", "80");
+
+        putDataRow(conn, tableName, "Rose", "info", "student_id", "20210000000004");
+        putDataRow(conn, tableName, "Rose", "info", "class", "2");
+        putDataRow(conn, tableName, "Rose", "score", "understanding", "60");
+        putDataRow(conn, tableName, "Rose", "score", "programming", "61");
+
+        putDataRow(conn, tableName, "zjx", "info", "student_id", "G20200343130026");
+        putDataRow(conn, tableName, "zjx", "info", "class", "1");
+        System.out.println("Data insertion success");
+
+        Table table = conn.getTable(tableName);
+        Scan scan = new Scan();
+        ResultScanner scanner = table.getScanner(scan);
+        Result result = null;
+        while ((result = scanner.next()) != null) {
+            List<Cell> cells = result.listCells();
+            System.out.print(Bytes.toString(CellUtil.cloneRow(cells.get(0))) + " => ");
+            for (Cell cell : cells) {
+                System.out.print(Bytes.toString(CellUtil.cloneFamily(cell)) + " {" + Bytes.toString(CellUtil.cloneQualifier(cell)) + ":" + Bytes.toString(CellUtil.cloneValue(cell)) + "} ");
+            }
+            System.out.println();
+        }
+
+        Delete delete = new Delete(Bytes.toBytes("Jerry"));
+        conn.getTable(tableName).delete(delete);
+        System.out.println("Jerry deletion Success");
+        
+        Get get = new Get(Bytes.toBytes("Rose"));
+        if (!get.isCheckExistenceOnly()) {
+            Result result1 = conn.getTable(tableName).get(get);
+            System.out.print("FIND Rose => ");
+            for (Cell cell : result1.rawCells()) {
+                System.out.print(Bytes.toString(CellUtil.cloneFamily(cell)) + " {" + Bytes.toString(CellUtil.cloneQualifier(cell)) + ":" + Bytes.toString(CellUtil.cloneValue(cell)) + "}");
+            }
+            System.out.println("Data get success");
+        }
+
+        if (admin.tableExists(tableName)) {
+            admin.disableTable(tableName);
+            admin.deleteTable(tableName);
+            System.out.println("Table Delete Successful");
+        } else {
+            System.out.println("Table does not exist!");
+        }
+
+        if (findNamespace(admin, ns)) {
+            admin.deleteNamespace(ns);
+            System.out.println("Namespace Delete Successful");
+        } else {
+            System.out.println("Namespace does not exist!");
+        }
+    }
+    /*public static void main(String[] args) throws IOException {
+        Configuration configuration = HBaseConfiguration.create();
+        configuration.set("hbase.zookeeper.quorum", "emr-worker-2,emr-worker-1,emr-header-1");
+        configuration.set("hbase.zookeeper.property.clientPort", "2181");
+        configuration.set("hbase.master", "127.0.0.1:60000");
+        Connection conn = ConnectionFactory.createConnection(configuration);
+        Admin admin = conn.getAdmin();
+
+        TableName tableName = TableName.valueOf("zhangj_student");
+        String[] colFamilies = {"info", "score"};
+        if (admin.tableExists(tableName)) {
+            System.out.println("Table already exists");
+        } else {
+            TableDescriptorBuilder tdBuilder = TableDescriptorBuilder.newBuilder(tableName);
+            ColumnFamilyDescriptorBuilder cfdBuilder;
+            ColumnFamilyDescriptor cfd;
+            for (String colFamily : colFamilies) {
+                cfdBuilder = ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes(colFamily));
+                cfd = cfdBuilder.build();
+                tdBuilder.setColumnFamily(cfd);
+            }
+            TableDescriptor td = tdBuilder.build();
+            admin.createTable(td);
+            System.out.println("Table create successful");
+        }
+    }*/
+}
